78313b17cd1554c57156b508b02e233690a555fe
Itay Levi <itaylh33@gmail.com>
2025-12-26 17:34:14 +0200

    useable version

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-const grid = document.querySelector("#grid")
+const grid = document.querySelector(".grid");
+const gridAreaBtn = document.querySelector("#grid-area");
 
-for (let i = 0; i < 16*16; i++) {
-    const div = document.createElement("div")
-    grid.appendChild(div)
+function makeGrid(area) {
+    const divs = grid.querySelectorAll("div")
+    divs.forEach((div) => {
+        div.remove()
+    })
+    for (let i = 0; i < (area * area); i++) {
+        const div = document.createElement("div");
+        div.style.flexBasis = `${800/area}px`;
+        grid.appendChild(div);
+        div.addEventListener("mouseenter", () => {
+            div.style.background = "blue";
+        });
+    }
 }
+makeGrid(16)
+
+function replaceGrid(input) {
+    makeGrid(input)
+}
+
+gridAreaBtn.addEventListener("click", () => {
+    const input = parseInt(prompt("Enter the number of squares per side:"));
+    if (input > 100) alert("please enter a number lower then 100");
+    else {
+        replaceGrid(input);
+    }
+});
